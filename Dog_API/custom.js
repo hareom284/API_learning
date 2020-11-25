40cc654b053d97d3c56dff835aaa9a32805da2e3
hareom284 <zawzawwin@ucsloikaw.edu.mm>
2020-11-25 20:36:33 +0630

    Dog API using fetch() in js

---
+++ Dog_API/custom.js
@@ -0,0 +1,18 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const promise = fetch(DOG_URL);
+const doggos = document.querySelector(".container");
+
+promise
+  .then(function(response) {
+    const processingPromise = response.json();
+    console.log(processingPromise);
+    return processingPromise;
+    // console.log(processingPromise);
+  })
+  .then(function(processedResponse) {
+    const img = document.createElement("img");
+    img.src = processedResponse.message;
+    img.alt = "Cute doggo";
+    doggos.appendChild(img);
+  });
